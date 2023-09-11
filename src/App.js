7e28929435aa32d2e5adb5923e321b364652e9ce
Seol_JY <wlsdud5654@gmail.com>
2023-09-12 00:17:35 +0900

    :sparkles: 모달 확인용 코드 작성
    - 각 모달별 Open  버튼 작성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,70 @@
+import modalStore from "./stores/modalStore";
+import ModalContainer from "./modals/ModalContainer";
+import SignInModal from "./modals/SignInModal/SignInModal";
+import SettingModal from "./modals/SettingModal/SettingModal";
+import SignUpModal from "./modals/SignUpModal/SignUpModal";
+import HistoryModal from "./modals/HistoryModal/HistoryModal";
+import DetailModal from "./modals/DetailModal/DetailModal";
+import EditModal from "./modals/EditModal/EditModal";
 import "./App.css";
 
 const App = () => {
-  return <div className="App"> </div>;
+  const { setModal } = modalStore();
+  return (
+    <div className="App">
+      <ModalContainer />
+      <button
+        type="button"
+        onClick={() => {
+          setModal(<SignInModal />);
+        }}
+      >
+        로그인
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setModal(<SignUpModal />);
+        }}
+      >
+        회원가입
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setModal(<SettingModal />);
+        }}
+      >
+        설정
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setModal(<HistoryModal />);
+        }}
+      >
+        히스토리
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setModal(<EditModal />);
+        }}
+      >
+        작성 폼
+      </button>
+
+      <button
+        type="button"
+        onClick={() => {
+          setModal(<DetailModal />);
+        }}
+      >
+        일정 상세
+      </button>
+      <div>sdfkasldjflksajklfdsjklfjkl</div>
+    </div>
+  );
 };
 
 export default App;
