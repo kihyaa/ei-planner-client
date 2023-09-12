b2df75b580141ee5942b391b10f1646c9af49a6a
Seol_JY <wlsdud5654@gmail.com>
2023-09-12 20:42:54 +0900

    :sparkels: Modal 퍼블리싱 완료

--- src/modals/SignUpModal/SignUpModal.jsx
+++ src/modals/SignUpModal/SignUpModal.jsx
@@ -1,6 +1,8 @@
 import ModalHeader from "../components/ModalHeader";
 import ModalTextInput from "../components/ModalTextInput";
 import ModalWelcomeNotice from "../components/ModalWelcomeNotice";
+import ModalButton from "../components/ModalButton";
+import ModalSupportLink from "../components/ModalSupportLink";
 import "../../styles/modals/SignUpModal/SignUpModal.css";
 
 const SignUpModal = () => {
@@ -10,15 +12,17 @@ const SignUpModal = () => {
       <div className="modal-contents">
         <ModalWelcomeNotice />
         <div className="signup-input-wrapper">
-          <ModalTextInput placeholder="이메일" errMsg="이메일 형식이 아닙니다" />
+          <ModalTextInput placeholder="이메일" />
           <div className="signup-input-hr" />
           <ModalTextInput placeholder="닉네임" />
         </div>
-        <div className="signup-input-wrapper">
+        <div className="signup-input-wrapper-last">
           <ModalTextInput placeholder="비밀번호" />
           <div className="signup-input-hr" />
           <ModalTextInput placeholder="비밀번호 확인" />
         </div>
+        <ModalButton contents="가입하기" />
+        <ModalSupportLink infoMsg="계정이 있나요?" linkMsg="로그인하기" onClick={() => {}} />
       </div>
     </div>
   );
