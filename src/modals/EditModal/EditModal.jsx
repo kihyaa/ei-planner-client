cd062f5fba942b8df0074cdc264c224f330f2015
0Jaemin0 <jaemin2167@naver.com>
2023-09-13 01:54:15 +0900

    :sparkles: 일정 수정 EditModal 구현
    - 경우에 따라 일정 등록 or 일정 수정이 나오도록 구현
    - 일정 수정일 때 일정 정보 받아오는 부분 추후 구현
    - 삼항 연산자를 사용해 경우에 따라 다른 메세지 출력

--- src/modals/EditModal/EditModal.jsx
+++ src/modals/EditModal/EditModal.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CloseIcon from "../components/CloseIcon";
 import ModalTextInput from "../components/ModalTextInput";
 import ModalButton from "../components/ModalButton";
@@ -7,11 +7,28 @@ import Toggle from "../components/Toggle";
 import CalenderIcon from "../../assets/modals/Calender.svg";
 import "../../styles/modals/EditModal/EditModal.css";
 
-const EditModal = () => {
+const EditModal = ({ schedule }) => {
   const [selected, setSelected] = useState(true);
   const [deadline, setDeadLine] = useState("비어 있음");
+  // 수정일시 받아올 정보 저장
+  const [editStatus, setEditStatus] = useState();
+
+  const editForm = schedule || "default";
+
+  useEffect(() => {
+    if (schedule === "default") {
+      // 등록 폼
+    } else {
+      // 수정 폼 getSchedule() 실행
+    }
+  });
 
   useDidMountEffect(() => {}, [selected]);
+
+  const getSchedule = () => {
+    // axios 사용해서 일정 정보 받아오기
+    // setEditStatus로 정보 저장
+  };
 
   const openCalender = () => {
     // 달력 출력, setDeadLine, selected 전달
@@ -21,15 +38,15 @@ const EditModal = () => {
     <div className="modal modal-edit-height">
       <div className="modal-edit">
         <div className="modal-edit-title">
-          <div className="edit-text">일정 등록하기</div>
+          <div className="edit-text">{editForm === "default" ? "일정 등록하기" : "일정 수정하기"}</div>
           <CloseIcon />
         </div>
         <div className="edit-input-wrapper">
           <div className="edit-input-newSchedule">
-            <ModalTextInput placeholder="새 일정" />
+            <ModalTextInput placeholder={editForm === "default" ? "새 일정" : "받아온 정보"} />
           </div>
           <div className="edit-input-hr" />
-          <textarea placeholder="설명" />
+          <textarea placeholder={editForm === "default" ? "설명" : "받아온 정보"} />
         </div>
         <div className="edit-deadline">
           <div className="edit-deadline-calender">
@@ -37,14 +54,14 @@ const EditModal = () => {
             마감 일시
           </div>
           <div className="edit-deadline-date">
-            <div className="deadline-date">{deadline}</div>
+            <div className="deadline-date">{editForm === "default" ? "비어 있음" : "받아온 정보"}</div>
             <div className="deadline-time">
               시간 포함
               <Toggle selected={selected} setSelected={setSelected} />
             </div>
           </div>
         </div>
-        <ModalButton variant="contained" size="lg" contents="등록" />
+        <ModalButton variant="contained" size="lg" contents={editForm === "default" ? "등록" : "수정하기"} />
       </div>
     </div>
   );
