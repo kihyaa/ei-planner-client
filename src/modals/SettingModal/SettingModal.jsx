cabf2faeda5bc1685c4bedc0ceac03199945b8af
Seol_JY <wlsdud5654@gmail.com>
2023-09-12 14:19:59 +0900

    :sparkles: toggle 사용 관련 예제

--- src/modals/SettingModal/SettingModal.jsx
+++ src/modals/SettingModal/SettingModal.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import ModalHeader from "../components/ModalHeader";
+import Toggle from "../components/Toggle";
 import "../../styles/modals/SettingModal/SettingModal.css";
+import useDidMountEffect from "../../hooks/useDidMountEffect";
 
 const SettingModal = () => {
+  // useState 기본 값으로 설정 정보를 세팅해야함(원래 사용자 설정 상 토글이 켜져 있었는지)
+  const [selected, setSelected] = useState(true);
+
+  // useEffect와 사용 방법이 같으나, 첫 렌더링 시에 실행되지 않고, 상태가 변경될 때만 실행됨
+  useDidMountEffect(() => {
+    // Do Something, ex) 설정 변경을 axios put
+  }, [selected]);
+
   return (
     <div className="modal modal-responsive-height modal-setting-height">
       <ModalHeader title="설정" />
-      설정모달입니다
+      <div className="modal-contents">
+        <Toggle selected={selected} setSelected={setSelected} />
+      </div>
     </div>
   );
 };
