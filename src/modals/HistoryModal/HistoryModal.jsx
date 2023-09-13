843227e85374876de9fe779b51c344ce84665931
0Jaemin0 <jaemin2167@naver.com>
2023-09-13 20:18:03 +0900

    :sparkles: 히스토리 Modal 구현
    - 히스토리에 추가된 일정 정보를 보여주는 HistoryModal 구현
    - 히스토리 정보 컴포넌트를 사용해 일정 정보 제공

--- src/modals/HistoryModal/HistoryModal.jsx
+++ src/modals/HistoryModal/HistoryModal.jsx
@@ -1,11 +1,24 @@
 import ModalHeader from "../components/ModalHeader";
+import ModalButton from "../components/ModalButton";
+import HistoryInfo from "./components/HistoryInfo";
 import "../../styles/modals/HistoryModal/HistoryModal.css";
 
 const HistoryModal = () => {
   return (
     <div className="modal modal-responsive-height modal-history-height">
       <ModalHeader title="히스토리" />
-      히스토리모달입니다
+      <div className="modal-history">
+        <div className="history-title">
+          총&nbsp;<span>32</span>개의 히스토리
+          <ModalButton variant="outLine-default" size="sm" contents="전체 삭제" />
+        </div>
+        <div className="history-wrapper">
+          <HistoryInfo />
+          <HistoryInfo />
+          <HistoryInfo />
+          <HistoryInfo />
+        </div>
+      </div>
     </div>
   );
 };
