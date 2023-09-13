bb581b9d9ca9556fb9236dc08e87b4b974b8ea71
Seol_JY <wlsdud5654@gmail.com>
2023-09-14 05:49:00 +0900

    :sparkles: 설정 모달 기능 구현 1차 완료
    - 프로필 사진 업로드 제외 전 기능 개발 완료

--- src/modals/SettingModal/SettingModal.jsx
+++ src/modals/SettingModal/SettingModal.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import userStore from "../../stores/userStore";
 import ModalHeader from "../components/ModalHeader";
 import Toggle from "../components/Toggle";
 import useDidMountEffect from "../../hooks/useDidMountEffect";
@@ -6,71 +8,159 @@ import Avatar from "../../components/Avatar";
 import ModalButton from "../components/ModalButton";
 import UserInfoSetting from "./components/UserInfoSetting";
 import GithubIcon from "../../assets/github.svg";
+import DropDown from "./components/DropDown";
 import "../../styles/modals/SettingModal/SettingModal.css";
 
 const SettingModal = () => {
+  const { isAuthenticated, profileImageUrl, nickname, email, setUserData, clearUserData, setProfileImageUrl } =
+    userStore();
+
   // useState 기본 값으로 설정 정보를 세팅해야함(원래 사용자 설정 상 토글이 켜져 있었는지)
-  const [selected, setSelected] = useState(true);
+  const [toggleSelected, setToggleSelected] = useState(false);
+  const [urgentDate, setUrgentDate] = useState(0);
+
+  useEffect(() => {
+    const getSettingInfo = async () => {
+      try {
+        const res = await axios.get(`${process.env.REACT_APP_PROXY}settings`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        setToggleSelected(res.data.datetime_display);
+        setUrgentDate(res.data.auto_urgent);
+      } catch (error) {
+        window.location.reload();
+      }
+    };
+    getSettingInfo();
+  }, []);
 
   // useEffect와 사용 방법이 같으나, 첫 렌더링 시에 실행되지 않고, 상태가 변경될 때만 실행됨
   useDidMountEffect(() => {
-    // Do Something, ex) 설정 변경을 axios put
-  }, [selected]);
+    const fetchToggle = async () => {
+      try {
+        await axios.put(
+          `${process.env.REACT_APP_PROXY}settings/datetime-display`,
+          {
+            is_display_date_time: toggleSelected,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          },
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchToggle();
+  }, [toggleSelected]);
+
+  useDidMountEffect(() => {
+    const fetchDropdown = async () => {
+      try {
+        await axios.put(
+          `${process.env.REACT_APP_PROXY}settings/auto-urgent`,
+          {
+            auto_urgent_day: urgentDate,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          },
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchDropdown();
+  }, [urgentDate]);
+
+  const deleteUser = async () => {
+    try {
+      await axios.delete(`${process.env.REACT_APP_PROXY}auth/delete`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeProfileImg = async () => {
+    try {
+      const res = await axios.delete(`${process.env.REACT_APP_PROXY}member/profile-image`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      setProfileImageUrl(res.data.data.profile_image_url);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="modal modal-responsive-height modal-setting-height">
       <ModalHeader title="설정" />
-      <div className="modal-contents">
-        <div className="setting-info-container">
-          <div className="setting-profile-container">
-            <Avatar
-              username="Seol"
-              dimensions="5.563rem"
-              imgSrc="https://news.samsungdisplay.com/wp-content/uploads/2018/08/8.jpg"
-            />
-            <div className="modal-profile-btn-wrapper1">
-              <ModalButton size="sm" contents="프로필 업로드" />
+      {isAuthenticated && (
+        <div className="modal-contents">
+          <div className="setting-info-container">
+            <div className="setting-profile-container">
+              <Avatar username={nickname} dimensions="5.563rem" imgSrc={profileImageUrl} />
+              <div className="modal-profile-btn-wrapper1">
+                <ModalButton size="sm" contents="프로필 업로드" />
+              </div>
+              <div className="modal-profile-btn-wrapper2">
+                <ModalButton size="sm" variant="outLine-default" contents="이미지 제거" onClick={removeProfileImg} />
+              </div>
             </div>
-            <div className="modal-profile-btn-wrapper2">
-              <ModalButton size="sm" variant="outLine-default" contents="이미지 제거" />
+            <UserInfoSetting nickname={nickname} email={email} />
+          </div>
+          <div className="setting-list">
+            <div className="setting-list-info">
+              <h3>날짜 및 시간 표시</h3>
+              <p>일정 블록의 날짜 및 시간 표시 여부를 설정합니다.</p>
+            </div>
+            <div className="setting-list-action">
+              {" "}
+              <Toggle selected={toggleSelected} setSelected={setToggleSelected} />
             </div>
           </div>
-          <UserInfoSetting />
-        </div>
-        <div className="setting-list">
-          <div className="setting-list-info">
-            <h3>날짜 및 시간 표시</h3>
-            <p>일정 블록의 날짜 및 시간 표시 여부를 설정합니다.</p>
+          <div className="setting-list">
+            <div className="setting-list-info">
+              <h3>자동으로 긴급 전환</h3>
+              <p>설정 일자가 지나면 자동으로 일정 블록을 긴급으로 전환합니다.</p>
+            </div>
+            <div className="setting-list-action">
+              <DropDown value={urgentDate} setValue={setUrgentDate} />
+            </div>
           </div>
-          <div className="setting-list-action">
-            {" "}
-            <Toggle selected={selected} setSelected={setSelected} />
+          <div className="setting-list" style={{ border: "none" }}>
+            <div className="setting-list-info">
+              <h3>회원 탈퇴</h3>
+              <p>탈퇴 시 작성하신 일정이 모두 삭제되며 복구되지 않습니다.</p>
+            </div>
+            <div className="setting-list-action">
+              <ModalButton size="sm" variant="warning" contents="탈퇴 하기" onClick={deleteUser} />
+            </div>
           </div>
-        </div>
-        <div className="setting-list">
-          <div className="setting-list-info">
-            <h3>자동으로 긴급 전환</h3>
-            <p>설정 일자가 지나면 자동으로 일정 블록을 긴급으로 전환합니다.</p>
-          </div>
-          <div className="setting-list-action"> </div>
-        </div>
-        <div className="setting-list" style={{ border: "none" }}>
-          <div className="setting-list-info">
-            <h3>회원 탈퇴</h3>
-            <p>탈퇴 시 작성하신 일정이 모두 삭제되며 복구되지 않습니다.</p>
-          </div>
-          <div className="setting-list-action">
-            <ModalButton size="sm" variant="warning" contents="탈퇴 하기" />
-          </div>
-        </div>
 
-        <div className="setting-copy-container">
-          <p>Copyright © 2023 kihyaa All right reserved.</p>
-          <a href="https://github.com/kihyaa/" target="_blank" rel="noopener noreferrer">
-            <img src={GithubIcon} alt="깃허브" />
-          </a>
+          <div className="setting-copy-container">
+            <p>Copyright © 2023 kihyaa All right reserved.</p>
+            <a href="https://github.com/kihyaa/" target="_blank" rel="noopener noreferrer">
+              <img src={GithubIcon} alt="깃허브" />
+            </a>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/modals/SettingModal/components/UserInfoSetting.jsx
+++ src/modals/SettingModal/components/UserInfoSetting.jsx
@@ -1,22 +1,63 @@
 import { useState } from "react";
 import ModalTextInput from "../../components/ModalTextInput";
 import "../../../styles/modals/SettingModal/components/UserInfoSetting.css";
+import axios from "axios";
 import ModalButton from "../../components/ModalButton";
+import userStore from "../../../stores/userStore";
 
-const UserInfoSetting = () => {
+const UserInfoSetting = ({ nickname, email }) => {
+  const { setNickname } = userStore();
   const [isUsernameEdit, setIsUsernameEdit] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const [newNickname, setNewNickname] = useState(nickname);
+
+  const putNickname = async () => {
+    if (nickname === newNickname) {
+      setIsUsernameEdit(false);
+      return;
+    }
+    try {
+      const res = await axios.put(
+        `${process.env.REACT_APP_PROXY}member/nickname?nickname=${encodeURIComponent(newNickname)}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        },
+      );
+      setNickname(res.data.data.nickname);
+      setIsUsernameEdit(false);
+    } catch (error) {
+      if (error.response.data.message === "EXIST_USERNAME") {
+        setErrMsg("중복된 닉네임입니다");
+      } else {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <div className="user-info-setting-container">
       {isUsernameEdit ? (
-        <ModalTextInput size="sm" placeholder="닉네임" />
+        <ModalTextInput
+          size="sm"
+          placeholder="닉네임"
+          value={newNickname}
+          errMsg={errMsg}
+          onChange={(e) => {
+            setNewNickname(e.target.value);
+            setErrMsg("");
+          }}
+        />
       ) : (
-        <h2 className="user-info-setting-username">Jinyeong Seol</h2>
+        <h2 className="user-info-setting-username">{nickname}</h2>
       )}
-      <p className="user-info-setting-email">wlsdud5654@gmail.com</p>
+      <p className="user-info-setting-email">{email}</p>
       <div className="user-info-setting-opt">
         {isUsernameEdit ? (
           <div className="user-info-setting-save-btn">
-            <ModalButton size="sm" contents="저장" />
+            <ModalButton size="sm" contents="저장" onClick={putNickname} />
           </div>
         ) : (
           <button type="button" className="user-info-setting-edit-btn" onClick={() => setIsUsernameEdit(true)}>
