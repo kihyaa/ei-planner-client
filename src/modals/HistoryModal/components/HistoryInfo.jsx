7b9fc9f9645fcf8fedc4e36daf7f918ce14aff78
0Jaemin0 <jaemin2167@naver.com>
2023-09-13 22:31:17 +0900

    :sparkles: 히스토리 정보 컴포넌트 구현
    - 일정 상황을 전달 받아 각 상황마다 다른 색상 출력

--- src/modals/HistoryModal/components/HistoryInfo.jsx
+++ src/modals/HistoryModal/components/HistoryInfo.jsx
@@ -1,11 +1,9 @@
 import "../../../styles/modals/HistoryModal/components/HistoryInfo.css";
 
-const HistoryInfo = () => {
-  const test = "pending";
-
+const HistoryInfo = ({ situation }) => {
   return (
     <div className="hisotry-info">
-      <div className={`history-status ${test}`} />
+      <div className={`history-status ${situation}`} />
       <div className="history-info-wrapper">
         <div className="history-info-title">타이틀 부분</div>
         <div className="history-info-content">설명 부분</div>
