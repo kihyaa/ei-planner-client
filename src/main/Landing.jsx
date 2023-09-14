f8bf269f1521e6ba57c6e6fc78d6b0ead0ef6417
0Jaemin0 <jaemin2167@naver.com>
2023-09-15 02:33:23 +0900

    :sparkles: 랜딩 페이지 구현
    - 서비스의 간단한 설명을 볼 수 있는 랜딩 페이지 구현
    - 회원 가입 및 로그인 모달 동작

--- src/main/Landing.jsx
+++ src/main/Landing.jsx
@@ -1,5 +1,43 @@
+import modalStore from "../stores/modalStore";
+import SignInModal from "../modals/SignInModal/SignInModal";
+import SignUpModal from "../modals/SignUpModal/SignUpModal";
+import ServiceImg from "../assets/main/Service.svg";
+import "../styles/main/Landing.css";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  const { setModal } = modalStore();
+
+  return (
+    <div className="landing-page">
+      <div className="landing-wrapper">
+        <div className="landing-img">
+          <img src={ServiceImg} alt="닫기" />
+        </div>
+        <div className="landing-name">Ei Planner</div>
+        <div className="landing-description">아이플래너에서 작업의 우선순위를 지정하고 생산성을 높여보세요</div>
+        <button
+          type="button"
+          onClick={() => {
+            setModal(<SignUpModal />);
+          }}
+        >
+          시작하기
+        </button>
+        <div className="landing-login">
+          계정이 있나요?
+          <span
+            role="button"
+            tabIndex={0}
+            onClick={() => {
+              setModal(<SignInModal />);
+            }}
+          >
+            로그인하기
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
