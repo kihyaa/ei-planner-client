08aaefee40d2dab56b9a9b104e40b42f7b5dab65
0Jaemin0 <jaemin2167@naver.com>
2023-09-14 14:56:40 +0900

    :sparkles: 히스토리 Modal 구현
    - 사용자의 히스토리 정보를 불러오는 동작 구현
    - 히스토리에 존재하는 일정 전체 삭제 동작 구현
    - 히스토리 정보 컴포넌트를 사용해 일정 제공

--- src/modals/HistoryModal/HistoryModal.jsx
+++ src/modals/HistoryModal/HistoryModal.jsx
@@ -1,28 +1,64 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import ModalHeader from "../components/ModalHeader";
 import ModalButton from "../components/ModalButton";
 import HistoryInfo from "./components/HistoryInfo";
 import "../../styles/modals/HistoryModal/HistoryModal.css";
 
 const HistoryModal = () => {
+  const [historyInfo, setHistoryInfo] = useState();
+
+  useEffect(() => {
+    getHistory();
+  }, []);
+
+  const getHistory = async () => {
+    try {
+      const res = await axios.get(`${process.env.REACT_APP_PROXY}history`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      setHistoryInfo(res.data);
+      console.log(res);
+    } catch (error) {
+      alert("실패했습니다. 다시 시도해 주세요.");
+      console.error(error.message);
+    }
+  };
+
+  const onDeleteClick = async () => {
+    try {
+      const res = await axios.delete(`${process.env.REACT_APP_PROXY}history/clean `, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      console.log(res);
+      getHistory();
+    } catch (error) {
+      alert("실패했습니다. 다시 시도해 주세요.");
+      console.error(error.message);
+    }
+  };
   return (
     <div className="modal modal-responsive-height modal-history-height">
       <ModalHeader title="히스토리" />
       <div className="modal-history">
         <div className="history-title">
-          총&nbsp;<span>32</span>개의 히스토리
-          <ModalButton variant="outLine-default" size="sm" contents="전체 삭제" />
+          총&nbsp;<span>{historyInfo?.count}</span>개의 히스토리
+          <ModalButton variant="outLine-default" size="sm" contents="전체 삭제" onClick={onDeleteClick} />
         </div>
         <div className="history-wrapper">
-          <HistoryInfo situation="important_not_urgent" />
-          <HistoryInfo situation="not_important_urgent" />
-          <HistoryInfo situation="important_urgent" />
-          <HistoryInfo situation="not_important_not_urgent" />
-          <HistoryInfo situation="not_important_urgent" />
-          <HistoryInfo situation="important_urgent" />
-          <HistoryInfo situation="important_urgent" />
-          <HistoryInfo situation="important_not_urgent" />
-          <HistoryInfo situation="not_important_not_urgent" />
-          <HistoryInfo situation="not_important_not_urgent" />
+          {historyInfo?.count !== 0 &&
+            historyInfo?.task.map((data, index) => (
+              <HistoryInfo
+                title={data.title}
+                description={data.description}
+                date={data.completed_at}
+                situation={data.eiType}
+              />
+            ))}
         </div>
       </div>
     </div>
