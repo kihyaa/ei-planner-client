9758d569b7967048015cb3c0f9e6b9cdc6dff7cf
OhSeChan <seb_s19@naver.com>
2023-09-16 01:36:34 +0900

    :sparkles: dnd 핸들러 함수 추가 및 데이터 설정 기능
    - useTaskStore에서 task, getTask를 가져와 useEffect를 사용해 마운트 시
      전체 일정 조회를 수행한 뒤 client state 값인 items에 할당
    - DndContext에 사용될 dnd 핸들러 함수 handleDragStart, handleDragCancel,
      handelDragOver, handleDragEnd 와 두 container 사이의 요소 변경을 위한
      moveBetweenContainers 함수 구현

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,10 +1,148 @@
+import { useState, useEffect } from "react";
+import {
+  DndContext,
+  DragOverlay,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { sortableKeyboardCoordinates, arrayMove } from "@dnd-kit/sortable";
+import { useTaskStore } from "../stores/taskStore";
 import '../styles/main/Main.css';
 
 const Main = () => {
+  
+  const { task, getTask, getTaskById } = useTaskStore();
+
+  const [items, setItems] = useState(null);
+  const [activeId, setActiveId] = useState(null);
+  
+  useEffect(()=>{
+    getTask();
+    task && setItems(task);
+  },[]);
+  
+  const sensors = useSensors(
+    useSensor(MouseSensor),
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const handleDragStart = ({ active }) => {
+    setActiveId(active.id);
+  };
+
+  const handleDragCancel = () => {
+    setActiveId(null);
+  };
+
+  const moveBetweenContainers = (
+    items,
+    activeContainer,
+    activeIndex,
+    overContainer,
+    overIndex,
+    item
+  ) => {
+    const tempItems = JSON.parse(JSON.stringify(items));
+    const [targetItem] = tempItems[activeContainer].splice(activeIndex, 1);
+    tempItems[overContainer].splice(overIndex, 0, targetItem);
+
+    return tempItems;
+  };
+
+  const handleDragOver = ({ active, over }) => {
+    const overId = over?.id;
+
+    if (!overId) {
+      return;
+    }
+
+    const activeContainer = active.data.current.sortable.containerId;
+    const overContainer = over.data.current?.sortable.containerId || over.id;
+
+    if (activeContainer !== overContainer) {
+      setItems((items) => {
+        const activeIndex = active.data.current.sortable.index;
+
+        const overIndex =
+          over.id in items
+            ? items[overContainer].length + 1
+            : over.data.current.sortable.index;
+
+        return moveBetweenContainers(
+          items,
+          activeContainer,
+          activeIndex,
+          overContainer,
+          overIndex,
+          active.id
+        );
+      });
+    }
+  };
+
+  const handleDragEnd = ({ active, over }) => {
+    if (!over) {
+      setActiveId(null);
+      return;
+    }
+
+    if (active.id !== over.id) {
+      const activeContainer = active.data.current.sortable.containerId;
+      const overContainer = over.data.current?.sortable.containerId || over.id;
+      const activeIndex = active.data.current.sortable.index;
+      const overIndex =
+        over.id in items
+          ? items[overContainer].length + 1
+          : over.data.current.sortable.index;
+
+      setItems((items) => {
+        let newItems;
+        if (activeContainer === overContainer) {
+          newItems = {
+            ...items,
+            [overContainer]: arrayMove(
+              items[overContainer],
+              activeIndex,
+              overIndex
+            ),
+          };
+        } else {
+          newItems = moveBetweenContainers(
+            items,
+            activeContainer,
+            activeIndex,
+            overContainer,
+            overIndex,
+            active.id
+          );
+        }
+
+        return newItems;
+      });
+    }
+
+    setActiveId(null);
+  };
+
   return (
     <div className='content-layout'>
       <div className='content-layout-container'>
-        content
+        <div className="content">
+          <DndContext
+            sensors={sensors}
+            onDragStart={handleDragStart}
+            onDragCancel={handleDragCancel}
+            onDragOver={handleDragOver}
+            onDragEnd={handleDragEnd}>
+              todo, matrix
+          </DndContext>
+        </div>
       </div>
     </div>
   );
